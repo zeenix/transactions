cdd482458570849e0a6a12c748ab32c246fc6bdd
Zeeshan Ali <zeeshanak@gnome.org>
2021-05-03 14:15:51 +0200

    A simple intergration test

--- src/main.rs
+++ src/main.rs
@@ -82,3 +82,29 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod integration {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let data = "\
+            type,       client, tx, amount
+            deposit,    1,      1,  1.0
+            deposit,    2,      2,  2.0
+            deposit,    1,      3,  2.0
+            withdrawal, 1,      4,  1.5
+            withdrawal, 2,      5,  3.0\
+        ";
+        let mut output: Vec<u8> = vec![];
+        run(data.as_bytes(), &mut output);
+        let output = String::from_utf8(output).unwrap();
+        const EXPECTED_OUTPUT: &str = "\
+            client,available,held,total,locked\n\
+            1,1.5,0.0,1.5,false\n\
+            2,2.0,0.0,2.0,false\n\
+        ";
+        assert_eq!(output, EXPECTED_OUTPUT);
+    }
+}
